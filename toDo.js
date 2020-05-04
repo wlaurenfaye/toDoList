a936136458639a32412c4907c78bf162d533e28a
Lauren <wlaurenfaye@gmail.com>
2020-05-04 17:22:13 +0100

    add list item function js

---
+++ toDo.js
@@ -0,0 +1,38 @@
+//selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//event listeners
+todoButton.addEventListener('click', addTodo);
+
+
+
+
+//functions
+function addTodo(event){
+    //prevent form from submitting
+    event.preventDefault();
+    //todo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+    //LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hi' ;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    //check button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML='<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+
+    //trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML='<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+    //apprnd to listeners
+    todoList.appendChild(todoDiv);
+}
